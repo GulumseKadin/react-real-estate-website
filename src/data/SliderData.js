c02ef043a7131f4149bb82d8542511065e3a0bb3
serifhan <isikliserifhan@gmail.com>
2021-08-26 00:02:37 +0300

    navbar coding finish and herosection  started

---
+++ src/data/SliderData.js
@@ -0,0 +1,67 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+
+
+export const SliderData=[
+    { 
+        title: 'Luxury Villa in İstanbul, Turkey',
+        price: '$5,000,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageOne,
+        alt : 'House'
+    },
+    { 
+        title: 'Luxury Villa in İzmir, Turkey',
+        price: '$3,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageTwo,
+        alt : 'House'
+    },
+    { 
+        title: 'Luxury Villa in Bursa, Turkey',
+        price: '$3,000,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageThree,
+        alt : 'House'
+    },
+    { 
+        title: 'Luxury Villa in Mugla, Turkey',
+        price: '$4,000,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageFour,
+        alt : 'House'
+    }
+];
+
+/*export const SliderDataTwo=[
+    { 
+        title: 'House in İstanbul, Turkey',
+        price: '$5,000,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageFive,
+        alt : 'Mouse'
+    },
+    { 
+        title: 'House in İzmir, Turkey',
+        price: '$3,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageSix,
+        alt : 'Mouse'
+    },
+    { 
+        title: 'House in Bursa, Turkey',
+        price: '$3,000,000',
+        path: '/homes',
+        label: 'View Home',
+        image : ImageSeven,
+        alt : 'Mouse'
+    },
+];*/
